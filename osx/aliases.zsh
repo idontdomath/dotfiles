62d32be4f46b5ca94e62fe2813a252bcf67b7103
idontdomath <ramosmejiamartin@gmail.com>
2017-01-12 15:51:00 -0300

    osx alias to flush dns.

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,3 +1,4 @@
 alias wifi-up="networksetup -setairportpower en0 on"
 alias wifi-down="networksetup -setairportpower en0 off"
 alias trash-empty='rm -R ~/.Trash/*'
+alias flush-dns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
